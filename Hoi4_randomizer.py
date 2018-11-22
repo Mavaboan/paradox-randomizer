8d7c0645fe75a0d9440a4a1d26dddf8dbdad3fdb
Mathias Andersen <mavaboan@gmail.com>
2018-11-22 12:50:03 +0100

    More countries added about halfway trough

--- Hoi4_randomizer.py
+++ Hoi4_randomizer.py
@@ -4,7 +4,11 @@ print("Hello world!")
 
 #Still need to add a lot of countrys
 #The different countrys numbers added automaticly and starts with 0
-country = [
+countries = [
     "Denmark", "Norway", "Finland", "UK", "USA", "Soviet Union", 
-    "Poland",  
+    "Poland",  "Ireland", "Sweden", "Germany", "France", "Spain", 
+    "Italy", "Japan", "Iceland", "Lithuania", "Estonia", "Latvia",
+    "Romania", "Yugoslavia", "Serbia", "Greece", "Albania", "Bulgaria",
+    "Hungary", "Croatia", "Slovakia", "Luxemborg", "Belgium", "Netherlands",
+    "Czechoslovakia", "Austria", "Portugal", "Switzerland", ""
 ]
