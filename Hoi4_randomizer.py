b8ac2c3ff8e2afa18695cfe7b44de6a8b21a4a10
Mathias Andersen <mavaboan@gmail.com>
2018-11-22 13:52:17 +0100

    Remaining countries added

--- Hoi4_randomizer.py
+++ Hoi4_randomizer.py
@@ -2,7 +2,7 @@ import random
 
 print("Hello world!")
 
-#Still need to add a lot of countrys
+#All countries added!!
 #The different countrys numbers added automaticly and starts with 0
 countries = [
     "Denmark", "Norway", "Finland", "UK", "USA", "Soviet Union", 
@@ -10,7 +10,16 @@ countries = [
     "Italy", "Japan", "Iceland", "Lithuania", "Estonia", "Latvia",
     "Romania", "Yugoslavia", "Serbia", "Greece", "Albania", "Bulgaria",
     "Hungary", "Croatia", "Slovakia", "Luxemborg", "Belgium", "Netherlands",
-    "Czechoslovakia", "Austria", "Portugal", "Switzerland", ""
+    "Czechoslovakia", "Austria", "Portugal", "Switzerland", "Afghanistan",
+    "Iraq", "Oman", "Iran", "Saudi Arabia", "Turkey", "Yemen", "Egypt",
+    "Syria", "Lebonon", "Jordan", "Libya", "Australia", "New Zealand", 
+    "Bhutan", "Tibet", "Nepal", "British Raj", "Philipines", "Siam",
+    "China", "Guangxi Clique", "Peoples Republic of China", "Shanxi",
+    "Xibei San Ma", "Sinkian", "Yunnan", "Mongolia", "Mengkukuo", "Tannu Tuva", 
+    "Korea", "Liberia", "Ethiopia", "South Africa", "Canada", "Mexico",
+    "Costa Rica", "El Salvadore", "Guatamala", "Honduras", "Nicaragua",
+    "Panama", "Cuba", "Dominican Republic", "Haiti", "Brazil", "Argentina",
+    "Chile", "Colombia", "Bolivia", "Ecuador", "Paraguay", "Peru", "Uruguay", "Venezuela"
 ]
 
 print(len(countries))
