c85f87d78b5fbffdf8bdbbf54b5ff5a22f8fc06e
Mathias Andersen <mavaboan@gmail.com>
2018-11-22 12:40:06 +0100

    Added some countrys still need more

--- Hoi4_randomizer.py
+++ Hoi4_randomizer.py
@@ -1,3 +1,10 @@
 import random
 
 print("Hello world!")
+
+#Still need to add a lot of countrys
+#The different countrys numbers added automaticly and starts with 0
+country = [
+    "Denmark", "Norway", "Finland", "UK", "USA", "Soviet Union", 
+    "Poland",  
+]
