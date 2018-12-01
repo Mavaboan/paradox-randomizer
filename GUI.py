3a4a5da34ff014af168653c310bf176d8561c039
Mavaboan <mavaboan@gmail.com>
2018-12-01 14:46:50 +0100

    Still learning, Wrote comments to everything new

--- GUI.py
+++ GUI.py
@@ -7,7 +7,7 @@ class myFrame(wx.Frame):
 
         #The exit button
         panel = wx.Panel(self)
-        button = wx.Button(panel, label = "exit", pos=(130,10), size=(152,152))
+        button = wx.Button(panel, label = "exit", pos=(130,10), size=(60,60))
         self.Bind(wx.EVT_BUTTON, self.closebutton, button)
         self.Bind(wx.EVT_CLOSE, self.closewindow)
 
@@ -23,6 +23,27 @@ class myFrame(wx.Frame):
         menubar.Append(first, "File")
         menubar.Append(second, "Edit")
         self.SetMenuBar(menubar)
+
+        #Creates a static text that is 10 pixels down and 10 pixels across
+        wx.StaticText(panel, -1, "This i static text", (10,10))
+
+        #Makes a variable called custom and creates a text which is 10 down 
+        #and 30 across and has colour 260 pixels across and is aligned left
+        custom = wx.StaticText(panel, -1, "this is custom", (10,30), (260,-1), wx.ALIGN_CENTER)
+        #Sets the custom variabel to have a text colour of white
+        custom.SetForegroundColour('white')
+        #Sets the custom variable to have the bakcgroundcolour to blue t
+        #This is where the 260 in the custom variable comes in
+        custom.SetBackgroundColour('blue')
+
+        panel = wx.Panel(self)
+        #User prompt that let's the user enter something
+        test = wx.TextEntryDialog(None, "Whats your name", "Title", "Enter name")
+        #if they clicked ok the data gets stored in apples
+        if test.ShowModal() == wx.ID_OK:
+            apples = test.GetValue()
+        #text panel which shows the data from apples
+        wx.StaticText(panel, -1, apples, (10,50))
 
 
     def closebutton(self, event):
