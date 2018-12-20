d206c73d976cdc16409a2ea819ac29cf953bfc80
Mathias Andersen <mavaboan@gmail.com>
2018-12-20 13:48:00 +0100

    lot

--- Hoi4_randomizer.py
+++ Hoi4_randomizer.py
@@ -24,7 +24,7 @@ countries = [
 country_amount = len(countries)
 #Finds a random number between 0 and the amount of countries we have.
 random_number = random.randint(0,country_amount)
-
+print(country_amount)
 #Sets up a function to return the flag file of the randomly selected country, based on which random number was found.
 def country_flag():
     if countries[random_number] == countries[0]:
@@ -73,120 +73,118 @@ def country_flag():
     elif countries[random_number] == countries[21]:
         return ("Flags\Relevant_flags\BUL_neutrality.png")
     elif countries[random_number] == countries[22]:
-        return ("Flags\Relevant_flags\HUN_fascism.png")
+        return ("Flags\Relevant_flags\HUN_neutrality.png")
     elif countries[random_number] == countries[23]:
-        return ("Flags\Relevant_flags\SLO_fascism.png")
-    elif countries[random_number] == countries[24]:
         return ("Flags\Relevant_flags\LUX_democratic.png")
-    elif countries[random_number] == countries[25]:
+    elif countries[random_number] == countries[24]:
         return ("Flags\Relevant_flags\BEL_democratic.png")
-    elif countries[random_number] == countries[26]:
+    elif countries[random_number] == countries[25]:
         return ("Flags\Relevant_flags\HOL_democratic.png")
-    elif countries[random_number] == countries[27]:
+    elif countries[random_number] == countries[26]:
         return ("Flags\Relevant_flags\CZE_democratic.png")
-    elif countries[random_number] == countries[28]:
+    elif countries[random_number] == countries[27]:
         return ("Flags\Relevant_flags\AUS_neutrality.png")
-    elif countries[random_number] == countries[29]:
+    elif countries[random_number] == countries[28]:
         return ("Flags\Relevant_flags\POR_neutrality.png")
-    elif countries[random_number] == countries[30]:
+    elif countries[random_number] == countries[29]:
         return ("Flags\Relevant_flags\SWI_democratic.png")
-    elif countries[random_number] == countries[31]:
+    elif countries[random_number] == countries[30]:
         return ("Flags\Relevant_flags\AFG_neutrality.png")
-    elif countries[random_number] == countries[32]:
+    elif countries[random_number] == countries[31]:
         return ("Flags\Relevant_flags\IRQ_neutrality.png")
-    elif countries[random_number] == countries[33]:
+    elif countries[random_number] == countries[32]:
         return ("Flags\Relevant_flags\OMA_neutrality.png")
-    elif countries[random_number] == countries[34]:
+    elif countries[random_number] == countries[33]:
         return ("Flags\Relevant_flags\PER_neutrality.png")
-    elif countries[random_number] == countries[35]:
+    elif countries[random_number] == countries[34]:
         return ("Flags\Relevant_flags\SAU_neutrality.png")
-    elif countries[random_number] == countries[36]:
+    elif countries[random_number] == countries[35]:
         return ("Flags\Relevant_flags\TUR_neutrality.png")
-    elif countries[random_number] == countries[37]:
+    elif countries[random_number] == countries[36]:
         return ("Flags\Relevant_flags\YEM_neutrality.png")
-    elif countries[random_number] == countries[38]:
+    elif countries[random_number] == countries[37]:
         return ("Flags\Relevant_flags\AUS_democratic.png")
-    elif countries[random_number] == countries[39]:
+    elif countries[random_number] == countries[38]:
         return ("Flags\Relevant_flags\\NZL_democratic.png")
-    elif countries[random_number] == countries[40]:
+    elif countries[random_number] == countries[39]:
         return ("Flags\Relevant_flags\BHU_neutrality.png")
-    elif countries[random_number] == countries[41]:
+    elif countries[random_number] == countries[40]:
         return ("Flags\Relevant_flags\TIB_neutrality.png")
-    elif countries[random_number] == countries[42]:
+    elif countries[random_number] == countries[41]:
         return ("Flags\Relevant_flags\\NEP_neutrality.png")
-    elif countries[random_number] == countries[43]:
+    elif countries[random_number] == countries[42]:
         return ("Flags\Relevant_flags\RAJ_neutrality.png")
-    elif countries[random_number] == countries[44]:
+    elif countries[random_number] == countries[43]:
         return ("Flags\Relevant_flags\PHI_democratic.png")
-    elif countries[random_number] == countries[45]:
+    elif countries[random_number] == countries[44]:
         return ("Flags\Relevant_flags\SIA_neutrality.png")
-    elif countries[random_number] == countries[46]:
+    elif countries[random_number] == countries[45]:
         return ("Flags\Relevant_flags\CHI_neutrality.png")
-    elif countries[random_number] == countries[47]:
+    elif countries[random_number] == countries[46]:
         return ("Flags\Relevant_flags\GXC_neutrality.png")
-    elif countries[random_number] == countries[48]:
+    elif countries[random_number] == countries[47]:
         return ("Flags\Relevant_flags\PRC_communism.png")
-    elif countries[random_number] == countries[49]:
+    elif countries[random_number] == countries[48]:
         return ("Flags\Relevant_flags\SHX_neutrality.png")
-    elif countries[random_number] == countries[50]:
+    elif countries[random_number] == countries[49]:
         return ("Flags\Relevant_flags\XSM_neutrality.png")
-    elif countries[random_number] == countries[51]:
+    elif countries[random_number] == countries[50]:
         return ("Flags\Relevant_flags\SIK_communism.png")
-    elif countries[random_number] == countries[52]:
+    elif countries[random_number] == countries[51]:
         return ("Flags\Relevant_flags\YUN_neutrality.png")
-    elif countries[random_number] == countries[53]:
+    elif countries[random_number] == countries[52]:
         return ("Flags\Relevant_flags\MON_communism.png")
-    elif countries[random_number] == countries[54]:
+    elif countries[random_number] == countries[53]:
         return ("Flags\Relevant_flags\MEN.png")
-    elif countries[random_number] == countries[55]:
+    elif countries[random_number] == countries[54]:
         return ("Flags\Relevant_flags\TAN_communism.png")
-    elif countries[random_number] == countries[56]:
+    elif countries[random_number] == countries[55]:
         return ("Flags\Relevant_flags\LIB_democratic.png")
-    elif countries[random_number] == countries[57]:
+    elif countries[random_number] == countries[56]:
         return ("Flags\Relevant_flags\ETH_neutrality.png")
-    elif countries[random_number] == countries[58]:
+    elif countries[random_number] == countries[57]:
         return ("Flags\Relevant_flags\SAF_democratic.png")
-    elif countries[random_number] == countries[59]:
+    elif countries[random_number] == countries[58]:
         return ("Flags\Relevant_flags\CAN_democratic.png")
-    elif countries[random_number] == countries[60]:
+    elif countries[random_number] == countries[59]:
         return ("Flags\Relevant_flags\MEX_neutrality.png")
-    elif countries[random_number] == countries[61]:
+    elif countries[random_number] == countries[60]:
         return ("Flags\Relevant_flags\COS_democratic.png")
-    elif countries[random_number] == countries[62]:
+    elif countries[random_number] == countries[61]:
         return ("Flags\Relevant_flags\ELS_fascism.png")
-    elif countries[random_number] == countries[63]:
+    elif countries[random_number] == countries[62]:
         return ("Flags\Relevant_flags\GUA_neutrality.png")
-    elif countries[random_number] == countries[64]:
+    elif countries[random_number] == countries[63]:
         return ("Flags\Relevant_flags\HON_democratic.png")
-    elif countries[random_number] == countries[65]:
+    elif countries[random_number] == countries[64]:
         return ("Flags\Relevant_flags\\NIC_neutrality.png")
-    elif countries[random_number] == countries[66]:
+    elif countries[random_number] == countries[65]:
         return ("Flags\Relevant_flags\PAN_democratic.png")
-    elif countries[random_number] == countries[67]:
+    elif countries[random_number] == countries[66]:
         return ("Flags\Relevant_flags\CUB_democratic.png")
-    elif countries[random_number] == countries[68]:
+    elif countries[random_number] == countries[67]:
         return ("Flags\Relevant_flags\DOM_fascism.png")
-    elif countries[random_number] == countries[69]:
+    elif countries[random_number] == countries[68]:
         return ("Flags\Relevant_flags\HAI_democratic.png")
-    elif countries[random_number] == countries[70]:
+    elif countries[random_number] == countries[69]:
         return ("Flags\Relevant_flags\BRA_neutrality.png")
-    elif countries[random_number] == countries[71]:
+    elif countries[random_number] == countries[70]:
         return ("Flags\Relevant_flags\ARG_neutrality.png")
-    elif countries[random_number] == countries[72]:
+    elif countries[random_number] == countries[71]:
         return ("Flags\Relevant_flags\CHL_democratic.png")
-    elif countries[random_number] == countries[73]:
+    elif countries[random_number] == countries[72]:
         return ("Flags\Relevant_flags\COL_democratic.png")
-    elif countries[random_number] == countries[74]:
+    elif countries[random_number] == countries[73]:
         return ("Flags\Relevant_flags\BOL_neutrality.png")
-    elif countries[random_number] == countries[75]:
+    elif countries[random_number] == countries[74]:
         return ("Flags\Relevant_flags\ECU_democratic.png")
-    elif countries[random_number] == countries[76]:
+    elif countries[random_number] == countries[75]:
         return ("Flags\Relevant_flags\PAR_communism.png")
-    elif countries[random_number] == countries[77]:
+    elif countries[random_number] == countries[76]:
         return ("Flags\Relevant_flags\PRU_fascism.png")
-    elif countries[random_number] == countries[78]:
+    elif countries[random_number] == countries[77]:
         return "Flags\Relevant_flags\YKR.png"
-    elif countries[random_number] == countries[79]:
+    elif countries[random_number] == countries[78]:
         return "Flags\Relevant_flags\VEN_fascism"
     else:
         print("can't find flag")
